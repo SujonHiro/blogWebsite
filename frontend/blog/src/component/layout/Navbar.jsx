9d8176431af7bc5598690d435d5b9f927fac1115
Sujon <57975469+SujonHiro@users.noreply.github.com>
2024-02-05 04:46:48 +0600

    error fixed

--- frontend/blog/src/component/layout/Navbar.jsx
+++ frontend/blog/src/component/layout/Navbar.jsx
@@ -73,8 +73,6 @@ const userNavigation = [
                                 </button>
                             </div>
                             {/*transition*/}
-
-                                
                                 {isDropdownOpen && (
                                     <ul className='absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
                                     {
